cf6b52a54897803dd739040edb828c7fdbcfe407
simplon-marseille <acardnicolas@hotmail.com>
2019-08-13 17:54:27 +0200

    firt commit project

---
+++ data.js
@@ -0,0 +1,65 @@
+const products = [
+{
+  id: '1',
+  name: 'Iphone6',
+  img: 'phone.jpg',
+  price: '600',
+  color: 'white',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+ id: '2',
+  name: 'Iphone5',
+  price: '600',
+  img: 'phone.jpg',
+  color: 'white',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+  id: '3',
+  name: 'Iphone4',
+  img: 'phone.jpg',
+  price: '400',
+  color: 'golden',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+  id: '4',
+  name: 'Iphone6',
+  img: 'phone.jpg',
+  price: '300',
+  color: 'gray',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+  id: '5',
+  name: 'Iphone6',
+  img: 'phone.jpg',
+  price: '300',
+  color: 'gray',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+  id: '6',
+  name: 'Iphone6',
+  img: 'phone.jpg',
+  price: '300',
+  color: 'gray',
+  company: 'apple',
+  description: 'lorem ipsum'
+},
+{
+  id: '7',
+  name: 'Iphone6',
+  img: 'phone.jpg',
+  price: '300',
+  color: 'gray',
+  company: 'apple',
+  description: 'lorem ipsum'
+}
+];
